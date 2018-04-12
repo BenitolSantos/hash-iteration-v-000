6786ca59108f02d3647476cd3de492de847caa84
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-12 19:45:55 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -3,15 +3,9 @@
 # suite_b: "Seymour Hoffman",
 # suite_c: "Alfred Tennyson",
 # suite_d: "Charlie Chaplin",
-# suite_e: "Crumpet the Elf" }
+# suite_e: "Crumpet the Elf"
+# }
 
 def select_winner(passengers)
   # add the code snippet here!
-  winner = ""
-  passengers.each do |suite, name|
-      if suite == :suite_a && name.start_With?("A")
-          winner = name
-      end
-  winner
-  end
 end
